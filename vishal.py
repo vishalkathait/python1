a43d0698719c1c49dd786d209ae5d73e585fb615
vishalkathait <kathaitvishal6@gmail.com>
2017-03-06 14:39:12 +0530

    Add files via upload

---
+++ vishal.py
@@ -0,0 +1,14 @@
+'''print(4*4)'''
+'''text ='vishal'
+saveFile=open('vishal.txt','w')
+saveFile.write(text)
+saveFile.close()'''
+'''appendMe ='singh'
+appendFile =open('vishal.txt','a')
+appendFile.write(appendMe)
+appendFile.close'''
+'''readMe = open('vishal.txt', 'r').read()
+print(readMe)'''
+x = raw_input('oye roll')
+print(x)
+
